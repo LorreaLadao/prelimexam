bbf009e6c744c47cf82d5ffe321c77f975d9be21
LorreaLadao <lfladao.student@ua.edu.ph>
2023-09-07 14:47:40 +0800

    second commit

--- pages/index.js
+++ pages/index.js
@@ -6,55 +6,44 @@ export default function Home() {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>Introducing Lorrea</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p> I am <b>Lorrea</b>, a unique name with <b>"Ladao"</b> as my surname. I've spent 20
               years in this world, residing in the town of Guagua. Though people often address
-              me as 'Lors,' the meaning of my name is 'red'. Introducing myself took time, as I
+              me as 'Lors,' the meaning of my name is <span style={{ color: 'red' }}> 'red'</span>.
+              Introducing myself took time, as I
               had to understand my strengths, weaknesses, and favorites. To provide a better
               grasp of who I am, here are the following details:
               <></>
           </p>
               <br></br>
               <p>
-              ❖ Favorite music: "Scott Street" by Phoebe Rodgers
-              Trivia: This song became my favorite two months ago, reminding me
-              of my experiences rising from rock-bottom and the new chapters
-              opening in my life.
+              <b>❖ Favorite music:</b> "Scott Street" by Phoebe Bridgers
               </p>
-              <br></br>
               <p>
-              ❖ Favorite food: asado, chicken curry, ice cream, cake, mochi, gummy
+              <b>❖ Favorite food: </b> asado, chicken curry, ice cream, cake, mochi, gummy
               foods
               </p>
-              <br></br>
               <p>
-              ❖ Background: I transitioned from being a victim to being the hero of my
+              <b>❖ Background:</b> I transitioned from being a victim to being the hero of my
               own story.
               </p>
-              <br></br>
               <p>
-              ❖ Hobbies: Writing novels, hand designing, and dancing
+              <b>❖ Hobbies:</b> Writing novels, hand designing, and dancing
               </p>
-              <br></br>
               <p>
-              ❖ Interests: Business investments, Planets, Universe, New discoveries
+              <b>❖ Interests:</b> Business investments, Planets, Universe, New discoveries
               </p>
-              <br></br>
               <p>
-              ❖ Strengths: Creativity, collaboration, productivity, compassion, intuitive
+              <b>❖ Strengths: </b> Creativity, collaboration, productivity, compassion, intuitive
               understanding of people, commitment, future-driven mindset, goal-
               oriented, organizational skills, discipline
               </p>
-              <br></br>
               <p>
-              ❖ Weaknesses: Communication skills, public speaking, time
+              <b>❖ Weaknesses: </b>Communication skills, public speaking, time
               management</p>
               <br></br>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        
       </section>
     </Layout>
   );
