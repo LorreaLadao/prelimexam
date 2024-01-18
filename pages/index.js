cc941d009f04d1457beb3f7d576cc05eab67b9c2
LorreaLadao <141934519+LorreaLadao@users.noreply.github.com>
2024-01-18 11:14:54 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,42 +9,13 @@ export default function Home() {
         <title>Introducing Lorrea</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p> I am <b>Lorrea</b>, a unique name with <b>"Ladao"</b> as my surname. I've spent 20
-              years in this world, residing in the town of Guagua. Though people often address
-              me as 'Lors,' the meaning of my name is <span style={{ color: 'red' }}> 'red'</span>.
-              Introducing myself took time, as I
-              had to understand my strengths, weaknesses, and favorites. To provide a better
-              grasp of who I am, here are the following details:
-              <></>
-          </p>
-          <hr></hr>
-              <br></br>
-              <p>
-              <b>❖ Favorite music:</b> "Scott Street" by Phoebe Bridgers
-              </p>
-              <p>
-              <b>❖ Favorite food: </b> asado, chicken curry, ice cream, cake, mochi, gummy
-              foods
-              </p>
-              <p>
-              <b>❖ Background:</b> I transitioned from being a victim to being the hero of my
-              own story.
-              </p>
-              <p>
-              <b>❖ Hobbies:</b> Writing novels, hand designing, and dancing
-              </p>
-              <p>
-              <b>❖ Interests:</b> Business investments, Planets, Universe, New discoveries
-              </p>
-              <p>
-              <b>❖ Strengths: </b> Creativity, collaboration, productivity, compassion, intuitive
-              understanding of people, commitment, future-driven mindset, goal-
-              oriented, organizational skills, discipline
-              </p>
-              <p>
-              <b>❖ Weaknesses: </b>Communication skills, public speaking, time
-              management</p>
-              <br></br>
+        <p> I am <b>Lorrea</b>, a unique name with <b>"Ladao"</b> as my surname. A student from University of the Assumption, Bachelor
+	of Science in Information Technology. I've spent 20 years in this world, residing in the town of Guagua.
+        </p>
+         <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
       </section>
     </Layout>
   );
