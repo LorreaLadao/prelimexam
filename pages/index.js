79c1475b3f04fd8fe0fa97348d4eecd020495a85
LorreaLadao <141934519+LorreaLadao@users.noreply.github.com>
2024-01-18 11:27:39 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,26 @@ export default function Home() {
         <title>Introducing Lorrea</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p> I am <b>Lorrea</b>, a unique name with <b>"Ladao"</b> as my surname. A student from University of the Assumption, Bachelor
+        <p> I am <b>Lorrea</b>, a unique name with <b>"Ladao"</b> as my surname. A student from University of the Assumption, taking up Bachelor
 	of Science in Information Technology. I've spent 20 years in this world, residing in the town of Guagua.
         </p>
+	<ul>
+        <li> I expect to learn more about web and client services as these terms are not new but it requires the fundamental topics for me to delve deeper as I don't have an idea yet on how it works.</li>
+        <li> The course may seem challenging but I do believe that our professor can make it easier for us to learn about it.</li>
+        <li> Looking forward into passing the exams and have a passing grades with enough knowledge.</li>        
+    </ul>
+    <ol>
+        <li> Punctuality </li>
+        <li> Accountability </li>
+        <li> Responsible </li>
+        <li> Loving Yourself </li>
+        <li> Attentively Listening </li>
+        <li> Doing Better </li>
+        <li> Appreciative </li>
+        <li> Healthy Living </li>
+        <li> Maintaining Positive Affirmations</li>
+        <li> Showing Concern </li>
+    </ol>
          <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
