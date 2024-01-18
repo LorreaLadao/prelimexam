0567c0b34e2701e89947c299e6893ca8be01efff
LorreaLadao <141934519+LorreaLadao@users.noreply.github.com>
2024-01-18 11:29:42 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,16 @@ export default function Home() {
         <p> I am <b>Lorrea</b>, a unique name with <b>"Ladao"</b> as my surname. A student from University of the Assumption, taking up Bachelor
 	of Science in Information Technology. I've spent 20 years in this world, residing in the town of Guagua.
         </p>
+	<br>
 	<ul>
+	<p> Expectations: </p>
         <li> I expect to learn more about web and client services as these terms are not new but it requires the fundamental topics for me to delve deeper as I don't have an idea yet on how it works.</li>
         <li> The course may seem challenging but I do believe that our professor can make it easier for us to learn about it.</li>
         <li> Looking forward into passing the exams and have a passing grades with enough knowledge.</li>        
     </ul>
+	<br>
     <ol>
+	<p> Things that require zero talent: </p>
         <li> Punctuality </li>
         <li> Accountability </li>
         <li> Responsible </li>
